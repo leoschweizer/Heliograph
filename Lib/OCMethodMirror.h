6bfeb5e133dd8d879b3e26404875d6f654a8fc37
Leo Schweizer <leoschweizer@users.noreply.github.com>
2016-01-03 17:33:52 +0100

    MethodMirror refactoring

--- Lib/OCMethodMirror.h
+++ Lib/OCMethodMirror.h
@@ -8,12 +8,38 @@
 
 @interface OCMethodMirror : NSObject
 
+/**
+ * The OCClassMirror reflecting the receiver's mirrored method's defining class.
+ */
 @property (nonatomic, readonly) OCClassMirror *definingClass;
-@property (nonatomic, readonly) Method mirroredMethod;
-@property (nonatomic, readonly) NSUInteger numberOfArguments;
-@property (nonatomic, readonly) SEL selector;
-@property (nonatomic, readonly) OCTypeMirror *returnType;
 
-- (instancetype)initWithDefiningClass:(OCClassMirror *)classMirror method:(Method)method;
+/**
+ * The reflected method.
+ */
+@property (nonatomic, readonly) Method mirroredMethod;
+
+/**
+ * Answers an instance of OCMethodMirror reflecting aMethod. Don't call this
+ * directly, use [reflect(...) methodDictionary] to retrieve instances of
+ * this class.
+ */
+- (instancetype)initWithDefiningClass:(OCClassMirror *)classMirror method:(Method)aMethod;
+
+/**
+ * Answers the number of arguments of the receiver's mirrored method, not
+ * including the implicit self and _cmd arguments.
+ */
+- (NSUInteger)numberOfArguments;
+
+/**
+ * Answers an OCTypeMirror reflecting the return type of the receiver's mirrored
+ * method.
+ */
+- (OCTypeMirror *)returnType;
+
+/**
+ * Answers the selector of the receiver's mirrored method.
+ */
+- (SEL)selector;
 
 @end
